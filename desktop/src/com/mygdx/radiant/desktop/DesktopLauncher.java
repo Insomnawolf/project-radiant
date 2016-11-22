b13aa7222d6043aa78bff3aff3b8b10720e45e9a
Edward Mondragon <Edward Mondragon>
2016-11-22 10:51:25 -0600

    Initial commit

---
+++ desktop/src/com/mygdx/radiant/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.radiant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.radiant.RadiantCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RadiantCore(), config);
+		config.width = 1920;
+		config.height = 1080;
+	}
+}
