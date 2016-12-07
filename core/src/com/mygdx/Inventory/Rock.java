30ff875bfc8c9554ffb2f3e32e34fbf23c8811b6
shadowraive <coolcliff892@Yahoo.com>
2016-12-06 19:06:02 -0600

    added more to rock class, still unfinished however

--- core/src/com/mygdx/Inventory/Rock.java
+++ core/src/com/mygdx/Inventory/Rock.java
@@ -2,6 +2,8 @@ package com.mygdx.Inventory;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by coolc on 12/5/2016.
@@ -9,9 +11,19 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Rock extends Item {
 
-    public Rock(){
+    public Rock(Boolean onMap, Vector2 position){
         name = "Rock";
         description = "Basic stone, pretty hard ...maybe";
         imgMap = new Texture(Gdx.files.internal("RockBasic.png"));
+        //if on the map render it using the map image
+        if(onMap)
+            sprite = new Sprite(imgMap);
+        else
+            sprite = new Sprite(imgUI);
+
+        //set its position on the screen
+        sprite.setPosition(position.x,position.y);
+
+
     }
 }
