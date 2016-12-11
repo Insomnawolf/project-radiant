9d32b3738869d8ad73583d86dc4f6a84d1da8a8b
shadowraive <coolcliff892@Yahoo.com>
2016-12-11 12:13:10 -0600

    added comments for gamescreen

--- core/src/com/mygdx/Inventory/Item.java
+++ core/src/com/mygdx/Inventory/Item.java
@@ -3,6 +3,7 @@ package com.mygdx.Inventory;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g3d.particles.influencers.DynamicsModifier;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Cliff on 12/5/2016.
@@ -14,12 +15,20 @@ public abstract class Item {
     protected Texture imgMap;                     // use this for displaying it on the map
     protected String description;                 // Items description
     protected Sprite sprite;                      // Sprite to represent the item
+    protected Vector2 position;
 
+    public Item(Vector2 position){
+        this.position = position;
+    }
     public String getName(){return name;}
     public String getDescription(){return description;}
     public void dispose(){
         imgMap.dispose();
         imgUI.dispose();
+    }
+
+    public void update(){
+        sprite.setPosition(position.x, position.y);
     }
 
 }
--- core/src/com/mygdx/graphics/screens/GameScreen.java
+++ core/src/com/mygdx/graphics/screens/GameScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+import com.mygdx.Inventory.Item;
 import com.mygdx.entities.Entity;
 import com.mygdx.graphics.renderer.OrthogonalTiledMapSpriteRenderer;
 import com.mygdx.entities.Player;
@@ -27,38 +28,71 @@ public class GameScreen extends ScreenAdapter{
     Player player;
     TiledMap tiledMap;
     OrthogonalTiledMapSpriteRenderer tiledMapRenderer;
-    private ArrayList<Entity> mapEntities = new ArrayList<Entity>();
+    private ArrayList<Entity> mapEntities = new ArrayList<Entity>();    //all entities that will be on the map
+    private ArrayList<Item> mapItems = new ArrayList<Item>();           //all items that will be on the map
+
+
     public GameScreen(RadiantCore game)
     {
+        //???
         this.game = game;
+
+        //Set up window bounds
         float width = Gdx.graphics.getWidth();
         float height = Gdx.graphics.getHeight();
+
+        //load the tile map
         tiledMap = new TmxMapLoader().load("test.tmx");
         tiledMapRenderer = new OrthogonalTiledMapSpriteRenderer(tiledMap);
         TiledMapTileLayer layer = (TiledMapTileLayer)tiledMap.getLayers().get(0);
         float tileWidth = layer.getTileWidth();
         float tileHeight = layer.getTileHeight();
+
+        //add in the player
         player = new Player(layer, new Vector2(layer.getWidth() * tileWidth / 2, layer.getHeight() * tileHeight / 2));
+
+        //set the camera
         camera = player.getCamera();
         camera.setToOrtho(false, width, height);
         camera.update();
+
+        //set up input listener
         input = new GameScreenInput(player, (TiledMapTileLayer)tiledMap.getLayers().get(0));
         Gdx.input.setInputProcessor(input);
+
+        //add player to tile map
         tiledMapRenderer.addSprite(player.getSprite());
         show();
     }
 
     @Override
     public void render(float delta) {
+
+        //Listen for inputs
         input.processInput();
+
+        //meh
         Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Update map objects
         player.update();
+        for(int i = 0; i < mapEntities.size(); i++){
+            mapEntities.get(i).update();
+        }
+        for(int i = 0; i < mapItems.size(); i++){
+            mapItems.get(i).update();
+        }
+
+        //Update camera
         camera.position.set(player.getPosX() + player.getSprite().getWidth()/2, player.getPosY() + player.getSprite().getHeight()/2, 0);
         camera.update();
+
+        //???
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
+
     }
 
     @Override
