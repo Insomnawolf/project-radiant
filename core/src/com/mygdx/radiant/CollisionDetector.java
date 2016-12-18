def15eb99ed23bac2be537fe64bb97beb7a7707e
shadowraive <coolcliff892@Yahoo.com>
2016-12-18 15:00:46 -0600

    Changes to collision detection, checks first, then moves

--- core/src/com/mygdx/radiant/CollisionDetector.java
+++ core/src/com/mygdx/radiant/CollisionDetector.java
@@ -18,7 +18,7 @@ public class CollisionDetector {
     private TiledMapTileLayer collisionLayer;
     private TiledMapTile tile;
 
-    private float oldX, oldY, tileWidth, tileHeight;
+    private float oldX, oldY, tileWidth, tileHeight, newX, newY;
 
     public CollisionDetector(Entity entity, TiledMapTileLayer collisionLayer) {
         this.entity = entity;
@@ -37,67 +37,93 @@ public class CollisionDetector {
         oldX = entity.getPosX();
         oldY = entity.getPosY();
         boolean collisionX = false, collisionY = false;
+        newX = entity.getPosX()+entity.getVelocityX();
 
-        entity.setPosX(entity.getPosX() + entity.getVelocityX());
+        //entity.setPosX(entity.getPosX() + entity.getVelocityX());
 
         if(entity.getVelocityX() < 0)
         {
             //top left tile collision test
-                collisionX = collisionLayer.getCell((int) (entity.getPosX() / tileWidth), (int) ((entity.getPosY() + entity.getSprite().getHeight()) / tileHeight)).getTile().getProperties().containsKey("blocked");
-                //middle left tile collision test
+                //collisionX = collisionLayer.getCell((int) (entity.getPosX() / tileWidth), (int) ((entity.getPosY() + entity.getSprite().getHeight()) / tileHeight)).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int) (newX / tileWidth), (int) ((entity.getPosY() + entity.getSprite().getHeight()) / tileHeight)).getTile().getProperties().containsKey("blocked");
+
+            //middle left tile collision test
             if(!collisionX)
-                collisionX = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+                //collisionX = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int)(newX/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+
             //bottom left tile collision test
             if(!collisionX)
-                collisionX = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+                //collisionX = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int)(newX/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
         }
         else if(entity.getVelocityX() > 0) {
             //top right tile collision test
-                collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+                //collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int)((newX + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
             //middle right tile collision test
             if(!collisionX)
-                collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+//                collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int)((newX + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight()/2)/tileHeight)).getTile().getProperties().containsKey("blocked");
+
             //bottom right tile collision test
             if(!collisionX)
-                collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+//                collisionX = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+                collisionX = collisionLayer.getCell((int)((newX + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+
         }
 
         if(collisionX)
         {
             entity.setVelocityX(0);
-            entity.setPosX(oldX);
+         //   entity.setPosX(oldX);
+        }else
+        {
+            entity.setPosX(newX);
         }
 
-        entity.setPosY(entity.getPosY() + entity.getVelocityY());
+        newY = entity.getPosY()+entity.getVelocityY();
+        //entity.setPosY(entity.getPosY() + entity.getVelocityY());
 
         if(entity.getVelocityY() < 0)
         {
             //bottom left tile collision test
-            collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+//            collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+              collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((newY/tileHeight))).getTile().getProperties().containsKey("blocked");
+
             //bottom middle tile collision test
             if(!collisionY)
-                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+//                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+              collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((newY/tileHeight))).getTile().getProperties().containsKey("blocked");
+
             //bottom right tile collision test
             if(!collisionY)
-                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+//                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY()/tileHeight))).getTile().getProperties().containsKey("blocked");
+                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((newY/tileHeight))).getTile().getProperties().containsKey("blocked");
 
         }
         else if(entity.getVelocityY() > 0)
         {
             //top left tile collision test
-            collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+//            collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+            collisionY = collisionLayer.getCell((int)(entity.getPosX()/tileWidth), (int)((newY + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
             //top middle tile collision test
             if(!collisionY)
-                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+//                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+            collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth()/2)/tileWidth), (int)((newY + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
             //top right tile collision test
             if(!collisionY)
-                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+//                collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((entity.getPosY() + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+               collisionY = collisionLayer.getCell((int)((entity.getPosX() + entity.getSprite().getWidth())/tileWidth), (int)((newY + entity.getSprite().getHeight())/tileHeight)).getTile().getProperties().containsKey("blocked");
+
         }
 
         if(collisionY)
         {
             entity.setVelocityY(0);
-            entity.setPosY(oldY);
+//            entity.setPosY(oldY);
+        }else{
+            entity.setPosY(newY);
         }
 
         return (collisionX || collisionY);
